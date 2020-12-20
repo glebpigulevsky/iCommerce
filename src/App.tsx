6e1ee73a16ff1ad486970655f4df14ea6b606aba
CasperCarver <pigulevsky.gleb@ya.ru>
2020-12-20 11:25:14 +0300

    CC-0-add-codeclimate: add upload coverage config

--- src/App.tsx
+++ src/App.tsx
@@ -6,19 +6,19 @@ import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 
-function App() {
-  return (
-    <Router>
-      <Header />
+const App = () => ( 
+  <Router>
+    <Header />
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact />
           <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
-      <Footer />
-    </Router>
-  )
-}
+    <Footer />
+  </Router>
+    );
+  
+
 
 export default App
