97cac72e19acbc739d9041c630d6638718f34d03
CasperCarver <pigulevsky.gleb@ya.ru>
2020-12-20 13:45:22 +0300

    CC-0-add-codeclimate: updated test app

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,24 +1,9 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import { Container } from 'react-bootstrap'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import HomeScreen from './screens/HomeScreen'
-import ProductScreen from './screens/ProductScreen'
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-const App = () => ( 
-  <Router>
-    <Header />
-      <main className='py-3'>
-        <Container>
-          <Route path='/' component={HomeScreen} exact />
-          <Route path='/product/:id' component={ProductScreen} />
-        </Container>
-      </main>
-    <Footer />
-  </Router>
-    );
-  
-
-
-export default App
+test('renders learn react link', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText('iCommerce');
+  expect(linkElement).toBeInTheDocument();
+});
