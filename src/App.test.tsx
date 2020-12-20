ad4e73a8c1c7b1c635c96b06a5c6da6f40ad2f20
CasperCarver <pigulevsky.gleb@ya.ru>
2020-12-20 13:13:39 +0300

    CC-0-add-codeclimate: updated test app

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+  );
   const linkElement = getByText('iCommerce');
   expect(linkElement).toBeInTheDocument();
 });
